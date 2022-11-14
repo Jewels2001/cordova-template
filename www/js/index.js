c6d8e80b66848565370d4e33d7a74d31bdbc9b93
Jewels2001 <juliewq25@gmail.com>
2022-11-14 11:42:16 -0700

    Add: working camera

--- www/js/index.js
+++ www/js/index.js
@@ -4,4 +4,35 @@ document.addEventListener('deviceready', onDeviceReady, false);
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
 
+    var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.FILE_URI
+    };
+
+    $("#takePhoto").on("click", takePic);
+
+    function takePic() {
+        console.log("click");
+        navigator.camera.getPicture(onSuccess, onError, options)
+    }
+
+    function onSuccess(imageData) {
+        console.log("Success! :)");
+        console.log(imageData);
+        resolveLocalFileSystemURL(imageData, function (fileEntry) {
+            var myNewImage = fileEntry.toURL()
+            console.log(myNewImage);
+            // do something with URL, assign to src or create an html 
+            //$('#photo1').href = myNewImage;
+            $("#takePhoto").after("<img src='" + myNewImage + "'>");
+            // $("#myimg").attr("src", imageData);
+            // $("#gallery").append("<img src='" + imgData + "'>");
+        }, onError);
+    }
+
+    function onError(message) {
+        console.log("Error :(");
+        alert("Photo not taken because " + message);
+    }
+
 }
